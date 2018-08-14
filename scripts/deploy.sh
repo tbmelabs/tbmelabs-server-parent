19a07256b6a1ab8d9cb3eb08205d11f30ed776e8
bbortt <timon.borter@post.ch>
2018-08-14 19:48:01 +0200

    prepare for shared project on github.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# Deployment script for TBME-TV
-# Code open source hosted on: https://github.com/tbmelabs/tbme-tv
+# Deployment script for tbmelabs-tv-constants
+# Code open source hosted on: https://github.com/tbmelabs/tbmelabs-tv-constants
 
 set -ev
 
 if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
-  # mvn versions:set deploy -DremoveSnapshot -DskipTests=true
-  echo "we'll deploy releases here!"
+  mvn versions:set deploy -DremoveSnapshot -DskipTests=true
 elif [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-  # mvn deploy -DskipTests=true
-  echo "we'll deploy snapshot here!"
+  mvn deploy -DskipTests=true
 fi
 
 exit $?
